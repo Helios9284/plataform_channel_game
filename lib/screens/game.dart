7d19909bbee89d4fae68524196a7a3a894c4121c
alaphy <alaphy.marcos@hotmail.com>
2022-09-12 20:58:06 -0300

    novo projeto 12.9

--- lib/screens/game.dart
+++ lib/screens/game.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:plataform_channel_game/constants/colors.dart';
+import 'package:plataform_channel_game/constants/styles.dart';
+import 'package:plataform_channel_game/models/creator.dart';
 
 class GameWindget extends StatefulWidget {
   const GameWindget({Key? key}) : super(key: key);
@@ -10,6 +12,9 @@ class GameWindget extends StatefulWidget {
 }
 
 class _GameWindget extends State<GameWindget> {
+  Creator? creator;
+  bool minhaVez = false;
+
   @override
   Widget build(BuildContext context) {
     ScreenUtil.init(context, designSize: const Size(700, 1400));
@@ -40,9 +45,42 @@ class _GameWindget extends State<GameWindget> {
                 color: colorDarkBlue3,
               )
             ],
+          ),
+          Container(
+            height: ScreenUtil().setHeight(1400),
+            child: Center(
+                child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                creator == null
+                    ? Row(mainAxisSize: MainAxisSize.min, children: [
+                        buildButton("Criar", true),
+                        SizedBox(width: 10),
+                        buildButton("Entrar", false),
+                      ])
+                    : InkWell(
+                        child: Text(
+                          minhaVez == true
+                              ? "Faça sua jogada"
+                              : "Aguarde sua vez",
+                          //g
+                        ),
+                      )
+              ],
+            )),
           )
         ],
       ),
     ));
   }
+
+  Widget buildButton(String label, bool owner) => Container(
+        width: ScreenUtil().setWidth(300),
+        child: ElevatedButton(
+            child: Padding(
+                padding: const EdgeInsets.all(8),
+                child: Text(label, style: textStyle36)),
+            onPressed: () {}),
+      );
 }
